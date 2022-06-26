255a04c6fe4c750e1a9eb21ae474c60887a45de7
PelagiaRod <43434069+PelagiaRod@users.noreply.github.com>
2022-06-27 01:04:50 +0300

    Delete app/src directory

--- app/src/TestConsumer.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.net.UnknownHostException;
-
-public class TestConsumer {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        Consumer consumer = new Consumer();
-        consumer.start();
-    }
-}
--- app/src/TestPublisher.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.net.UnknownHostException;
-
-public class TestPublisher {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        Publisher publisher = new Publisher();
-        publisher.start();
-    }
-}
